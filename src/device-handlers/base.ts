655ec7e38cf7ae704295ce865e3f59b6f66351ee
Alexander Ryden <alexander.ryden@gmail.com>
2022-01-12 07:40:18 +0100

    Move the ShellyAccessory class into DeviceHandler

--- src/device-handlers/base.ts
+++ src/device-handlers/base.ts
@@ -3,10 +3,37 @@ import { PlatformAccessory } from 'homebridge';
 
 import { Ability } from '../abilities';
 import { DeviceLogger } from '../utils/device-logger';
-import { ShellyAccessory } from '../accessory';
 import { PLATFORM_NAME, PLUGIN_NAME, ShellyPlatform } from '../platform';
 
 type AccessoryId = string;
+
+/**
+ * Represents a HomeKit accessory.
+ */
+export class ShellyAccessory {
+  /**
+   * Holds this accessory's abilities.
+   */
+  readonly abilities: Ability[];
+
+  /**
+   * @param platformAccessory - The underlying homebridge platform accessory.
+   * @param abilities - The abilities that this accessory has.
+   */
+  constructor(readonly platformAccessory: PlatformAccessory, ...abilities: Ability[]) {
+    this.abilities = abilities;
+  }
+
+  /**
+   * Removes all event listeners from this accessory.
+   */
+  detach() {
+    // invoke detach() on all abilities
+    for (const a of this.abilities) {
+      a.detach();
+    }
+  }
+}
 
 /**
  * A DeviceHandler manages accessories for a device.
