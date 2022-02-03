5fa617d9a721c9902dbcb3fb2c91afeb638f75a9
Alexander Ryden <alexander.ryden@gmail.com>
2022-02-03 07:50:10 +0100

    Add support for the Shelly Pro 1

---
+++ src/device-handlers/shelly-pro-1.ts
@@ -0,0 +1,21 @@
+import { ShellyPro1 } from 'shellies-ng';
+
+import { DeviceHandler } from './base';
+import { SwitchAbility } from '../abilities';
+
+/**
+ * Handles Shelly Pro 1 devices.
+ */
+export class ShellyPro1Handler extends DeviceHandler {
+  protected setup() {
+    const d = this.device as ShellyPro1;
+
+    this.createAccessory(
+      'switch',
+      null,
+      new SwitchAbility(d.switch0),
+    );
+  }
+}
+
+DeviceHandler.registerClass(ShellyPro1Handler, ShellyPro1.model);
