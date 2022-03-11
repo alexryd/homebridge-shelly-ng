1e0024ea7bd55dab06a50ccc4bc7071b5f692399
Alexander Ryden <alexander.ryden@gmail.com>
2022-03-11 06:23:28 +0100

    Add support for the Shelly Plus I4

--- src/device-handlers/index.ts
+++ src/device-handlers/index.ts
@@ -2,6 +2,7 @@ export * from './base';
 
 export * from './shelly-plus-1';
 export * from './shelly-plus-1-pm';
+export * from './shelly-plus-i4';
 export * from './shelly-pro-1';
 export * from './shelly-pro-1-pm';
 export * from './shelly-pro-2';
---
+++ src/device-handlers/shelly-plus-i4.ts
@@ -0,0 +1,42 @@
+import { ShellyPlusI4 } from 'shellies-ng';
+
+import { DeviceHandler } from './base';
+import {
+  ReadonlySwitchAbility,
+  ServiceLabelAbility,
+  StatelessProgrammableSwitchAbility,
+} from '../abilities';
+
+/**
+ * Handles Shelly Plus I4 devices.
+ */
+export class ShellyPlusI4Handler extends DeviceHandler {
+  protected setup() {
+    const d = this.device as ShellyPlusI4;
+
+    // determine each input type
+    const input0IsButton = d.input0.config?.type === 'button';
+    const input1IsButton = d.input1.config?.type === 'button';
+    const input2IsButton = d.input2.config?.type === 'button';
+    const input3IsButton = d.input3.config?.type === 'button';
+
+    // create an accessory for all button inputs
+    this.createAccessory(
+      'buttons',
+      null,
+      new StatelessProgrammableSwitchAbility(d.input0).setActive(input0IsButton),
+      new StatelessProgrammableSwitchAbility(d.input1).setActive(input1IsButton),
+      new StatelessProgrammableSwitchAbility(d.input2).setActive(input2IsButton),
+      new StatelessProgrammableSwitchAbility(d.input3).setActive(input3IsButton),
+      new ServiceLabelAbility(),
+    ).setActive(input0IsButton || input1IsButton || input2IsButton || input3IsButton);
+
+    // create accessories for all switch inputs
+    this.createAccessory('switch0', null, new ReadonlySwitchAbility(d.input0)).setActive(!input0IsButton);
+    this.createAccessory('switch1', null, new ReadonlySwitchAbility(d.input1)).setActive(!input1IsButton);
+    this.createAccessory('switch2', null, new ReadonlySwitchAbility(d.input2)).setActive(!input2IsButton);
+    this.createAccessory('switch3', null, new ReadonlySwitchAbility(d.input3)).setActive(!input3IsButton);
+  }
+}
+
+DeviceHandler.registerClass(ShellyPlusI4Handler, ShellyPlusI4.model);
