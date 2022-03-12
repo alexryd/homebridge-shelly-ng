2725c9028d3727b55b6fbf87a153d323602d8691
Alexander Ryden <alexander.ryden@gmail.com>
2022-03-12 06:03:22 +0100

    Add support for the Shelly Pro 2 PM cover profile

--- src/device-handlers/shelly-pro-2-pm.ts
+++ src/device-handlers/shelly-pro-2-pm.ts
@@ -1,7 +1,7 @@
 import { ShellyPro2Pm } from 'shellies-ng';
 
 import { DeviceHandler } from './base';
-import { SwitchAbility } from '../abilities';
+import { CoverAbility, SwitchAbility } from '../abilities';
 
 /**
  * Handles Shelly Pro 2 PM devices.
@@ -9,18 +9,25 @@ import { SwitchAbility } from '../abilities';
 export class ShellyPro2PmHandler extends DeviceHandler {
   protected setup() {
     const d = this.device as ShellyPro2Pm;
+    const isCover = d.profile === 'cover';
+
+    this.createAccessory(
+      'cover',
+      'Window',
+      new CoverAbility(d.cover0, 'window'),
+    ).setActive(isCover);
 
     this.createAccessory(
       'switch-0',
       'Switch 1',
       new SwitchAbility(d.switch0),
-    );
+    ).setActive(!isCover);
 
     this.createAccessory(
       'switch-1',
       'Switch 2',
       new SwitchAbility(d.switch1),
-    );
+    ).setActive(!isCover);
   }
 }
 
