18a818914b3acca21020fb119131b16eec029921
Swen <53879951+sw-koenig@users.noreply.github.com>
2023-10-21 19:22:44 +0000

    Added also support for Shelly Plus 1 PM Mini

--- src/device-delegates/shelly-plus-1-pm.ts
+++ src/device-delegates/shelly-plus-1-pm.ts
@@ -1,6 +1,7 @@
 import {
   ShellyPlus1Pm,
   ShellyPlus1PmUl,
+  ShellyPlus1PmMini,
 } from 'shellies-ng';
 
 import { DeviceDelegate } from './base';
@@ -20,4 +21,5 @@ DeviceDelegate.registerDelegate(
   ShellyPlus1PmDelegate,
   ShellyPlus1Pm,
   ShellyPlus1PmUl,
+  ShellyPlus1PmMini,
 );
